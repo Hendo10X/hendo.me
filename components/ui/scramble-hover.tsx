6e2fc6d1a2dbd74ca20ce3210bab94296e88bfcb
Henderson <hendersondike@gmail.com>
2026-04-02 01:36:47 +0100

    Remove unused useCallback import from ScrambleHover component

--- components/ui/scramble-hover.tsx
+++ components/ui/scramble-hover.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState, useRef, useCallback } from "react";
+import { useEffect, useState, useRef } from "react";
 import { motion } from "motion/react";
 
 import { cn } from "@/lib/utils";
